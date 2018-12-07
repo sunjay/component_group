e26a2c5add933f92f9ca48881a19a6540d21ea0f
Sunjay Varma <varma.sunjay@gmail.com>
2018-12-07 18:31:46 -0500

    Starting proc macro implementation

--- component_group_derive/src/lib.rs
+++ component_group_derive/src/lib.rs
@@ -1,22 +1,39 @@
 extern crate proc_macro;
-extern crate syn;
-
-#[macro_use]
-extern crate quote;
 
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{
+    parse_macro_input,
+    DeriveInput,
+    Data,
+    DataStruct,
+    DataEnum,
+    DataUnion,
+    token::{Enum, Union},
+    Fields,
+};
+use quote::quote;
 
 #[proc_macro_derive(ComponentGroup)]
 pub fn derive_component_group(input: TokenStream) -> TokenStream {
     // Parse the input tokens into a syntax tree
-    let input = parse_macro_input!(input as DeriveInput);
+    let DeriveInput {attrs, vis, ident, generics, data} = parse_macro_input!(input as DeriveInput);
 
-    // Build the output, possibly using quasi-quotation
-    let expanded = quote! {
-        // ...
-    };
+    match data {
+        Data::Struct(DataStruct {struct_token, fields, ..}) => match fields {
+            Fields::Named(fields) => TokenStream::from(quote! {
 
-    // Hand the output tokens back to the compiler
-    TokenStream::from(expanded)
+            }),
+            _ => {
+                syn::Error::new(struct_token.span, "Only structs with named fields are supported")
+                    .to_compile_error()
+                    .into()
+            },
+        },
+        Data::Enum(DataEnum {enum_token: Enum {span}, ..}) |
+        Data::Union(DataUnion {union_token: Union {span}, ..}) => {
+            syn::Error::new(span, "Only structs with named fields are supported")
+                .to_compile_error()
+                .into()
+        },
+    }
 }
